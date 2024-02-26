c701ca2a896fe5e25149ee962703874f27cbfc38
H-SM <harmanmalht777@gmail.com>
2024-02-26 23:41:37 +0530

    fix: added required dependences & integrated tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary-black': '#1A232E',
+        'secondary-white': '#c7c7c7',
+      },
+      transitionTimingFunction: {
+        'out-flex': 'cubic-bezier(0.05, 0.6, 0.4, 0.9)',
+      }
+    },
+  },
+  plugins: [],
+}
+
