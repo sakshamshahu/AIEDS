96cdec310a0cb627398cba659c3f9888b6988d2d
sakshamshahu <sakshamshahu.work@gmail.com>
2024-03-25 10:03:56 +0530

    fix: main file with dependeciy updates
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

---
+++ AI/ai/__main__.py
@@ -0,0 +1,22 @@
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings.sentence_transformer import (SentenceTransformerEmbeddings,)
+from langchain_community.vectorstores import Chroma
+loader = TextLoader('AI/ai/data/structured.pdf', encoding='UTF-8')
+documents = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+chunk_size=500, chunk_overlap=0, separators=[",", "\n"]
+)
+# text_splitter = CharacterTextSplitter(chunk_size=50, chunk_overlap=0, separators=[" ", ",", "\n"]) # check func of this
+docs = text_splitter.split_documents(documents)
+
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
+
+db = Chroma.from_documents(docs, embedding_function)
+
+query = "What is ventral stream?"
+docs = db.similarity_search(query)
+
+print(docs[0].page_content)
