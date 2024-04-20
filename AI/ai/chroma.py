7c26aae697b7505a1ece45fa3c637f6da510a819
sakshamshahu <sakshamshahu.work@gmail.com>
2024-04-20 16:01:42 +0530

    feat: parse txt
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- AI/ai/chroma.py
+++ AI/ai/chroma.py
@@ -1,7 +1,26 @@
 import chromadb
+import os
 
+def parse_file(filename):
+    with open(filename, encoding='utf-8-sig') as f:
+        paragraphs = []
+        buffer = []
+        for line in f.readlines():
+            line = line.strip()
+            if line:
+                buffer.append(line)
+            elif len(buffer):
+                paragraphs.append((' ').join(buffer))
+                buffer = []
+        if len(buffer): # if buffer is not empty after exiting loop append it to paragraphs
+            paragraphs.append((' ').join(buffer))
+        return paragraphs
 
 def main():
+    filename = 'ai/data/structured.txt'
+    paragraphs = parse_file(filename)
+    print(paragraphs[0],'\n',paragraphs[1])
+    
     # client = chromadb.PersistentClient(path="/path/to/save/to")
     chroma_client = chromadb.HttpClient(host='localhost', port=8000)
     
