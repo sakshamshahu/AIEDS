d42723c23b25e6d0239fc0f3bfb9bce2dd30269e
sakshamshahu <sakshamshahu.work@gmail.com>
2024-04-20 15:50:05 +0530

    feat: chroma collection
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- AI/ai/chroma.py
+++ AI/ai/chroma.py
@@ -2,7 +2,12 @@ import chromadb
 
 
 def main():
-    
     # client = chromadb.PersistentClient(path="/path/to/save/to")
     chroma_client = chromadb.HttpClient(host='localhost', port=8000)
+    
+    chroma_client.heartbeat()
+    
+    # Load a collection or create it if it doesn't exist
+    collection = chroma_client.get_or_create_collection(name="test", metadata={"hnsw:space": "cosine"})
+    
     print('Script Check')
