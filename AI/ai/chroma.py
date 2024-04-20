4064ac61989d8a2f05384e531e1fc28001b80c16
sakshamshahu <sakshamshahu.work@gmail.com>
2024-04-20 16:36:07 +0530

    feat: embed store chroma
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- AI/ai/chroma.py
+++ AI/ai/chroma.py
@@ -1,4 +1,5 @@
 import chromadb
+import ollama
 import os
 
 def parse_file(filename):
@@ -16,10 +17,20 @@ def parse_file(filename):
             paragraphs.append((' ').join(buffer))
         return paragraphs
 
+def get_embeddings(filename, modelname, chunks):
+    # checking for already saved embeddings
+    # if (embeddings := load_embeddings(filename)) is not False:
+    #     return embeddings
+    for index, chunk in enumerate(chunks):
+        embeddings = [
+            ollama.embeddings(model = modelname, prompt=chunk)['embedding']
+        ]
+        
+    return embeddings
+
 def main():
     filename = 'ai/data/structured.txt'
-    paragraphs = parse_file(filename)
-    print(paragraphs[0],'\n',paragraphs[1])
+    embed_model = 'nomic-embed-text'
     
     # client = chromadb.PersistentClient(path="/path/to/save/to")
     chroma_client = chromadb.HttpClient(host='localhost', port=8000)
@@ -28,5 +39,18 @@ def main():
     
     # Load a collection or create it if it doesn't exist
     collection = chroma_client.get_or_create_collection(name="test", metadata={"hnsw:space": "cosine"})
+    paragraphs = parse_file(filename)
+    # print(paragraphs[0],'\n',paragraphs[1])
+    
+    # embeddings = get_embeddings(filename, 'nomic-embed-text', paragraphs)
+    for index, chunk in enumerate(paragraphs):
+        embeddings = [
+            ollama.embeddings(model = embed_model, prompt=chunk)['embedding']
+        ]
+        collection.add([filename+str(index)], embeddings, documents=[chunk], metadatas={"source": filename})
+    
+    # print(type(embeddings), len(embeddings))
+    print(collection.peek())
+    
     
     print('Script Check')
