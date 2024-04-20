ea0bef125e761ef80678f9b24878f33102162e50
sakshamshahu <sakshamshahu.work@gmail.com>
2024-04-20 14:55:01 +0530

    chroma http client
    Signed-off-by: sakshamshahu <sakshamshahu.work@gmail.com>

--- AI/ai/chroma.py
+++ AI/ai/chroma.py
@@ -4,4 +4,5 @@ import chromadb
 def main():
     
     # client = chromadb.PersistentClient(path="/path/to/save/to")
+    chroma_client = chromadb.HttpClient(host='localhost', port=8000)
     print('Script Check')
