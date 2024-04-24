21dfe5f8062d6c11d9b15d929f0b4af2bda963e0
Sid-the-Slothh <sidhant.tomar003@gmail.com>
2024-04-24 14:05:41 +0530

    Fixed pdf_reader, added processed folder

--- AI/tests/pdf_reader.py
+++ AI/tests/pdf_reader.py
@@ -3,6 +3,9 @@ import pdf2image
 from PIL import Image
 import pytesseract
 import os
+import sys
+import time
+import collections
 
 def pdf_to_img(pdf_file):
     # Turns the pdf to images. PIL images.
@@ -13,7 +16,7 @@ def ocr_core(file):
     text = pytesseract.image_to_string(file)
     return text
 
-def print_pages(pdf_file, written_file):
+def convert_pages(pdf_file, written_file):
     # First convert pdf to image
     images = pdf_to_img(pdf_file)
     try:
@@ -24,22 +27,57 @@ def print_pages(pdf_file, written_file):
         pass
 
     # Then use the tesseract OCR to extract the text
-    with open(written_file, 'a') as f:
+    with open(written_file, 'w') as f:
 
         for pg, img in enumerate(images):
             # Each page is individually turned into text and written into a file called "text.txt"
             f.write("page " + str(pg) + ocr_core(img) + '\n')
 
 def main():
-    directory = './files'
-
+    directory = './files/'
+    otherdir = './processed/'
     # Iterate through all the files in the directory
-    for filename in os.listdir(directory):
-        filepath = os.path.join(directory, filename)
-        print(filepath)
-        if os.path.isfile(filepath):
-            print("hello", filepath)
-            print_pages(filepath, "./output/content.txt")
+    # Get the current working directory
+    current_dir = os.getcwd()
+
+    # Add the current working directory to the Python path
+    sys.path.append(current_dir)
+    while (1):
+        print("monitoring for pdfs!")
+        dirr = os.listdir(directory)
+        check = os.listdir(otherdir)
+        done = collections.defaultdict(bool)    # a hash map of files that already exist
+        for filename in check:
+            filepath = os.path.join(directory, filename)
+            
+            # if os.path.isfile(filepath):
+            fname = filepath.split("/")[-1]
+            fname = fname[:len(fname)-4]
+            
+            done[fname] = True
+
+        for filename in dirr:
+            filepath = os.path.join(directory, filename)
+
+            if (filepath == r"./files/.gitkeep"):
+                continue
+
+            if os.path.isfile(filepath):
+                fname = filepath.split("/")[-1]
+                fname = fname[:len(fname)-4]
+                if (done[fname]):   # remove the file if it already exists
+                    os.remove(filepath)
+                else:
+                    convert_pages(filepath, f"./output/{fname}.txt")
+
+                    # Construct the destination file path
+                    destination_file = os.path.join(otherdir, os.path.basename(filepath))
+
+                    # Move the file
+                    os.rename(filepath, destination_file)
+        time.sleep(3)
+        
+    
 
 
 # if __name__ == "__main__":
